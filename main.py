1d1c1905981f9ca06980721e9187b91d947f1786
veeeetus <dawbor589@gmail.com>
2024-05-13 13:10:38 +0200

    tweet

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_t
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-client.create_tweet(text = "Hello")
+def tweet():
+    client.create_tweet(text = "Hello")
